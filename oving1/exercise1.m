d7c62278ef579d6e45ffe668f4d2b44bb0f59fcb
Trygve Bærland <trygve.baerland@gmail.com>
2014-09-01 22:00:06 +0200

    Still issues with convergence, though.

--- oving1/exercise1.m
+++ oving1/exercise1.m
@@ -7,16 +7,17 @@ clear all;
 
 
 %Setting parameters and domain:
-eps = 0.1;
+eps = 0.05;
 N = ceil(0.5/eps);
 
 x = linspace(0,1,N+2);
 dx = x(2) - x(1);
 dt = min(0.25*dx^2, dx);
-nt = 1000;
+nt = 10000;
 
 %Setting initial condition:
-u0 = x(2:(end-1));
+deg = 2;
+u0 = 1/deg*x(2:(end-1)).^deg;
 %Stationary solution:
 u_stat = (exp(x/eps)-1)/(exp(1/eps)-1);
 
