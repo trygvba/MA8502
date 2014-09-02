b9b599d144e1fe3a899cbdeb38478698510b8ad4
trygvba <trygve.baerland@gmail.com>
2014-09-02 15:17:04 +0200

    Added implementation of central differences. Seems to work fine.

--- oving1/exercise1.m
+++ oving1/exercise1.m
@@ -7,28 +7,35 @@ clear all;
 
 
 %Setting parameters and domain:
-eps = 0.05;
-N = ceil(0.5/eps);
+eps = 0.01;
+N = ceil(0.5/eps)-1;
 
 x = linspace(0,1,N+2);
 dx = x(2) - x(1);
 dt = min(0.25*dx^2, dx);
-nt = 10000;
+T = 1;
+nt = ceil(T/dt);
 
 %Setting initial condition:
 deg = 2;
-u0 = 1/deg*x(2:(end-1)).^deg;
+u0 = x(2:(end-1)).^4;
 %Stationary solution:
 u_stat = (exp(x/eps)-1)/(exp(1/eps)-1);
 
 %Starting time integration:
 U_upwind = u0;
+U_central = u0;
 figure
 for i=1:nt
     plot(x,[0 U_upwind 1],'r')
     hold on
+    plot(x, [0 U_central 1], 'g')
     plot(x,u_stat,'b')
+    legend('Upwind','Central diff.', 'Stationary')
+    title(num2str(i/nt))
     hold off
     drawnow
     U_upwind = U_upwind -dt/dx * diff([0 U_upwind]) + eps*dt/dx^2 * diff([0 U_upwind 1],2);
+    U_central = U_central -dt/(2*dx) * ([U_central(2:end) 1] - [0 U_central(1:(end-1))]) + eps*dt/dx^2 * diff([0 U_central 1],2);
+    
 end
