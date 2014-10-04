e52e7f31818a80e472757857831cd1ae2fc82ba8
trygvba <trygve.baerland@gmail.com>
2014-10-04 17:47:40 +0200

    Made a function for total G-matrix over all points in the element.

--- Project/Laplace/laplace_functions.py
+++ Project/Laplace/laplace_functions.py
@@ -25,4 +25,27 @@ def G_matrix(xD, xTD, yD, yTD, alpha, beta):
     J = np.abs(x_xi*y_eta - x_eta*y_xi)
 
     return 1./J * np.array( [ [x_eta**2 + y_eta**2, -(y_eta*y_xi + x_eta*x_xi)],
-        [-(y_eta*y_xi + x_eta*x_xi), x_xi**2+y_xi**2] ])
+            [-(y_eta*y_xi + x_eta*x_xi), x_xi**2+y_xi**2] ])
+
+
+def assemble_total_G_matrix(xD, xTD, yD, yTD, nx, ny):
+    """Function calculating all the 2-by-2 G-matrices for all points in the element.
+    INPUT:
+        xD: Local x-coordinate matrix multiplied with lagrangian derivative matrix.
+        xTD: The transpose of the local x-matrix and the D-matrix.
+        yD, yTD: Analogously for the y-coordinates.
+    OUTPUT:
+        G: total G-matrix for all points in the element.
+    """
+    # Number of points in the element:
+    num_points = nx*ny
+
+    #Initialise the G-matrix:
+    G = np.zeros( (num_points, 2, 2) )
+
+    for I in range(num_points):
+        i = I/nx
+        j = I%nx
+        G[I] = G_matrix(xD, xTD, yD, yTD, i, j)
+
+    return G
