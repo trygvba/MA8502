fcb691735de421b5332c53fe407da69b13e8bfa1
Trygve Bærland <trygve.baerland@gmail.com>
2014-10-05 14:05:11 +0200

    Seems that the assembly stage takes the most time...

--- Project/Laplace/example_poisson_quarter_annulus.py
+++ Project/Laplace/example_poisson_quarter_annulus.py
@@ -31,22 +31,24 @@ def gamma4(xi):
     return 0.5*(3.+xi), 0.
 
 def f(x,y):
-    return x**2 + y**2
+    return 1.
 
-############################
-
+###########################
 # Order of GLL-points:
 N = 50
 xis = qn.GLL_points(N)
 weights = qn.GLL_weights(N, xis)
 
 #Generate mesh:
+print "Getting Mesh..."
 X, Y = gh.gordon_hall_grid( gamma1, gamma2, gamma3, gamma4, xis, xis)
 
 # Get lagrangian derivative matrix:
+print "Getting derivative matrix..."
 D = sg.diff_matrix(xis, N)
 
 # Get Jacobian and total G-matrix:
+print "Getting Jacobian and G-matrices."
 Jac, G_tot = lp.assemble_total_G_matrix(np.dot(X,D),
                                         np.dot(X.T,D),
                                         np.dot(Y,D),
@@ -56,11 +58,14 @@ Jac, G_tot = lp.assemble_total_G_matrix(np.dot(X,D),
   
 # Now we're closing in on some shit. Prepare to ASSEMBLE!
 # Assemble stiffness matrix:
+print "Assembling stiffness matrix."
 A = lp.assemble_local_stiffness_matrix(D, G_tot, N, weights)
 
 # Assemble loading vector:
+print "Assembling loading vector."
 F = lp.assemble_loading_vector(X, Y, f, Jac, weights)
 
+print "Imposing boundary conditions."
 # Now we need to handle the Dirichlet Boundary conditions:
 for i in range(N):
     #Lower side:
@@ -86,7 +91,9 @@ for i in range(N):
 ####################
 #   TIME TO SOLVE:
 ####################
+print "Solving..."
 U = la.solve(A, F)
+print "Done solving..."
 U = np.reshape ( U, (N,N) )
 
 ############################
